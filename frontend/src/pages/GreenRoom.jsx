2f83d15326ccfb453e48e48d435207474e709c0a
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2026-01-20 00:23:55 +0530

    feat(greenroom): add media preview using getUserMedia

--- frontend/src/pages/GreenRoom.jsx
+++ frontend/src/pages/GreenRoom.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 
@@ -11,6 +11,39 @@ function GreenRoom() {
   // this ref will hold our video element
   const videoRef = useRef(null);
 
+  //navigator.mediaDevices.getUserMedia() - this pops up the familiar permission request at the top of the screen asking to allow the use of camera and mic -
+  // since the function will return stream - means - continours flow of data (video frames and audio samples) coming straight from the hardware so we set **"constraints" object**
+  const constraints = {
+    video: true, 
+    audio: true
+  }
+
+  const [stream, setStream] = useState(null); //to manipulate the stream - like when user leaves or toggle 
+
+  useEffect(() => {
+    let localStream; 
+    const startStream = async () => {
+      try {
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        setStream(localStream);
+
+        if (videoRef.current) {
+          videoRef.current.srcObject = localStream;
+        }
+      } catch (error) {
+        console.error("Error accessing media devices.", error);
+      }
+    };
+
+    startStream();
+
+    // clean up --- Stopping the Tracks. Every mediaStream object has a method called getTracks() that returns an array of all active tracks (like your video and audio).
+    return () => {
+      if (localStream) {
+        localStream.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, [])
 
 
 
