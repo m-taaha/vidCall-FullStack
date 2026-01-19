faef45b56c19e38f35123ad4f385a58e9d057f5a
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2026-01-19 16:09:26 +0530

    feat: add meeting id to green room

--- frontend/src/pages/GreenRoom.jsx
+++ frontend/src/pages/GreenRoom.jsx
@@ -1,10 +1,22 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
 
 
 function GreenRoom() {
+  // destructure the 'id' property from useParams
+  const {id} = useParams();
+
   return (
-    <div>GreenRoom</div>
+    <div className='min-h-screen bg-slate-950 text-white p-8'>
+      <h1 className='text-3xl font-bold mb-4'>Green Room</h1>
+        <p>
+          Meeting ID: <span className='text-blue-400 font-mono'>{id}</span>
+        </p>
+
+
+        {/* TODO: will build the preview and controls here */}
+    </div>
   )
 }
 
